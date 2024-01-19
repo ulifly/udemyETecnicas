256747ed5cb6952039432e484c37642f4b925cd1
Ulifly <ulifly@hotmail.com>
2024-01-19 00:42:49 -0600

    se actualiza

---
+++ staircase.js
@@ -0,0 +1,7 @@
+// create a function that takes in a list of integers and returns them 
+// rearranged into a staircase, if possible, or False otherwise. 
+
+// A staircase is a list of lists where list 0 has length 1, 
+// and every list i+1 is one item longer than list i. 
+// The order of the elements in the staircase doesn’t matter.
+
